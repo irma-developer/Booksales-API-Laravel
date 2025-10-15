01a3389e9da7beaf332edd0e39d47b04321e8697
irma-developer <sitiirmawatiirma39@gmail.com>
2025-10-15 15:46:15 +0700

    Menggunakan API

--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -9,6 +9,10 @@ class GenreController extends Controller
     public function index()
     {
         $genres = Genre::all();   // ambil array dari Model
-        return view('genres.index', compact('genres'));
+        return response()->json([
+            "success" => true,
+            "message" => "Get all resources",
+            "data" => $genres
+        ], 200);
     }
 }
